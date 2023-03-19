b3b82f2654ccb4a0cdd07404cf01d1974a3c084d
aberiggs <sprocket710@gmail.com>
2023-03-19 03:21:14 -0400

    Google Maps added, and fetching current location.

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
+
+import React, { useState, useEffect } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
+import { PROVIDER_GOOGLE } from 'react-native-maps'
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let locationData = await Location.getCurrentPositionAsync({});
+
+      setLocation({
+        latitude: locationData.coords.latitude,
+        longitude: locationData.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      })
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView provider={PROVIDER_GOOGLE} 
+      initialRegion={location} 
+      style={styles.map} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +56,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
